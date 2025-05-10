e5c7bbb95914e357da43e06a2a573578b59117a7
trimiiii <trimkrasniqids@gmail.com>
2025-05-10 14:44:28 +0200

    hjedbfiud

---
+++ lesson26/main.js
@@ -0,0 +1,108 @@
+console.log("hello there")
+
+var x=5
+console.log(x)
+
+var y=132
+console.log(x+y)
+
+var a=90
+
+var b=43
+
+var c = a +b 
+
+console.log(c)
+
+var a=234
+
+console.log(a)
+
+
+n =10
+s =2
+rezultati= n%s
+
+console.log(rezultati)
+
+
+console.log(12%3)
+
+numri=26
+numri--;
+numri--;
+numri--;
+numri--;
+numri--;
+console.log(numri)
+
+
+numeri=23
+numri++;
+numri++;
+numri++;
+numri++;
+console.log(numeri)
+
+
+console.log(x)
+
+
+
+console.log("----------------")
+var y = 20
+y-=10
+console.log(y)
+
+
+console.log("----------------")
+var k = 20
+k*=10
+console.log(k)
+
+
+console.log("----------------")
+var l = 20
+
+
+
+console.log("----------Pjesa e dyte----------")
+
+var x =  5;
+var y = 5;
+
+console.log(x==y)
+
+
+
+
+
+
+
+console.log("----------------")
+
+var num1=150;
+var num2=200;
+
+console.log(num1>num2)
+
+console.log("---------------")
+console.log(num1<num2)
+
+
+
+
+var age = 36
+
+console.log(age>27 && age<78)
+
+var var1= 5;
+var var2= 6;
+
+console.log(var1==5 || var2==5)
+
+
+var firstName= "Dua"
+var lastName = "Spahiu"
+
+console.log(firstName+" "+lastName)
